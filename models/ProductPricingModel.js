5d057ed8505b60b7dcf1bd0b9d21c587c4a8fe47
bhagyashri3 <bhagyashri@nimapinfotech.com>
2021-10-07 14:33:35 +0530

    changes done

--- models/ProductPricingModel.js
+++ models/ProductPricingModel.js
@@ -20,24 +20,6 @@ exports.getAll = async function (data) {
        }
 },
 exports.save = async function (data) {
-    // var resBody
-    // var url = "http://localhost:3000/getAll"
-    // var options = {
-    //     method: "GET",
-    //     url: url,
-    //     headers: {
-    //         accessToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTVhOGU5MTZhZDE1MzllNjg2Mzk0MjciLCJuYW1lIjoiYmhhZ3lhc2hyaTc3NyIsImVtYWlsIjoiYmhhZ3lhc2hyaUBnbWFpbC5jb20iLCJtb2JpbGUiOiI5MjIxMjIxMjI0IiwiaWF0IjoxNjMzMzI0Njg5fQ.5XeywZOuBMX1tggdXWB01yURuQUR5F5tirBZaMwIiY4'
-    //     }
-    // }
-    // console.log(options)
-    // await request(options, function (err, response, body) {
-    //     if (err) {
-    //         return err, null
-    //     } else {
-    //         console.log("body============", body)
-    //         resBody = body.
-    //     }
-    // })
     let saveProduct
        let newProductObj = {
         productSku: data.productSku,
